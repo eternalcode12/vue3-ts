db2c82bb6746300cc9cead423f1ba7daaff858c1
eternalcoder <eternalcoder@163.com>
2022-08-03 10:34:36 +0800

    初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import ElementPlus from "unplugin-element-plus/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import WindiCSS from "vite-plugin-windicss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+    },
+    plugins: [
+        vue(),
+        ElementPlus(),
+        WindiCSS(),
+        AutoImport({
+            imports: ["vue", "vue-router", "vuex"],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+});
